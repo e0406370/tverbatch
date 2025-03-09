713eb7ee5ae10af6b3cfe1d93fc0040a58304e7e
e0406370 <e0406370@u.nus.edu>
2025-03-09 15:16:20 +0800

    [ci] update health workflow to include episode and series endpoints

--- .github/workflows/health.py
+++ .github/workflows/health.py
@@ -1,23 +1,28 @@
 from helpers.constants import Tver
 import requests, sys
 
+URL_LIST = [
+    Tver.BASE_URL,
+    Tver.get_episode_url(Tver.TEST_EPISODE["valid"]["id"]),
+    Tver.get_series_url(Tver.TEST_SERIES["valid"]["id"]),
+]
 
-def check_response(url: str) -> bool:
+
+def is_reachable(url: str) -> bool:
     try:
         response = requests.get(url)
 
         if response.status_code == 200:
-            print(f"Success: {url} is reachable.")
+            print(f"[Success] {url} is reachable.")
             return True
         else:
-            print(f"Warning: {url} returned status code {response.status_code}.")
+            print(f"[Warning] {url} returned status code {response.status_code}.")
             return False
 
     except requests.exceptions.RequestException as e:
-        print(f"Error occurred: {e}")
+        print(f"[Error] {e}")
         return False
 
 
 if __name__ == "__main__":
-    success = check_response(Tver.BASE_URL)
-    sys.exit(0 if success else 1)
+    sys.exit(0 if all(is_reachable(url) for url in URL_LIST) else 1)
